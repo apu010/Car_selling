f79055485a533c93f9551986e0d036e9df28129e
apu010 <apubhattacharjee2@gmail.com>
2022-01-30 07:30:36 -0800

    update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let manu = document.querySelector('#manu-btn');
+let navber = document.querySelector('.navbar');
+
+manu.onclick = () => {
+manu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick => {
+    document.querySelector('.login-from-container').classlist.toggle('active');
+
+}
