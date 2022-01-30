9730cc299d763e7260b3d9eb3fac4c22b232e326
apu010 <apubhattacharjee2@gmail.com>
2022-01-30 07:32:46 -0800

    update

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ manu.classList.toggle('fa-times');
 navbar.classList.toggle('active');
 }
 
-document.querySelector('#login-btn').onclick => {
+document.querySelector('#login-btn').onclick = () => {
     document.querySelector('.login-from-container').classlist.toggle('active');
 
 }
